137e3e1b9d94585491859fa154b8217430605ce4
JoGMG <joattah3@gmail.com>
2023-10-04 20:57:46 +0100

    flask stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,8 +2,33 @@
 """ Index File """
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
+from models.state import State
+from models.city import City
+from models.place import Place
+from models.user import User
+from models.amenity import Amenity
+from models.review import Review
 
 
 @app_views.route('/status')
 def status():
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats')
+def stats():
+    amenities = storage.count(Amenity)
+    cities = storage.count(City)
+    places = storage.count(Place)
+    reviews = storage.count(Review)
+    states = storage.count(State)
+    users = storage.count(User)
+    return jsonify({
+        "amenities": amenities,
+        "cities": cities,
+        "places": places,
+        "reviews": reviews,
+        "states": states,
+        "users": users
+    })
