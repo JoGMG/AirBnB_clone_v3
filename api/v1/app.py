80a4237c848a9515ffbbaaa823b32a11becc9645
JoGMG <joattah3@gmail.com>
2023-10-04 21:12:06 +0100

    not found error

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,7 +2,7 @@
 """ Starts the Flask Web Application """
 import os
 from models import storage
-from flask import Flask
+from flask import Flask, jsonify
 from api.v1.views import app_views
 
 app = Flask(__name__)
@@ -13,6 +13,11 @@ app.register_blueprint(app_views)
 def teardown(exc):
     """ Remove the current SQLAlchemy session """
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return jsonify({"error": "Not found"}), 404
 
 
 if __name__ == "__main__":
